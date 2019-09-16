52f1f4ffb6a3d014ae4dc84e6de997c6ec25c0a2
Joey Sabey <joey.sabey@gmx.com>
2019-09-16 06:15:47 +0100

    Added alternative CIDR_REGEX option for stricter matching

--- blacklist.py
+++ blacklist.py
@@ -18,6 +18,9 @@ CONFIG_FILE = 'config.toml'
 # on numbers >255 & subnets >32, but is probably faster than being more anal.
 CIDR_REGEX = '^(?:\d{1,3}\.){3}\d{1,3}(?:/\d{1,2})?$'
 
+# This alternative is very strict about what it considers 'correct', but is somewhat slower.
+#CIDR_REGEX = '^(?:(\d|[1-9]\d|1\d{2}|2[0-4]\d+|25[0-5])\.){3}(\d|[1-9]\d|1\d{2}|2[0-4]\d+|25[0-5])(?:\/([0-9]|[1-2]\d|3[0-2]))?$'
+
 # Make configuration settings globally available
 conf = toml.load(CONFIG_FILE)
 
